8d739b3ff4dbaf8bdeca029c869c084ac92a24c3
Will Byrne <will.alex.byrne@gmail.com>
2024-11-04 16:57:49 +0000

    make the / route serve an html file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import express, { Express } from 'express';
+import { join } from 'path';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (_, res) => {
   // serve the form as html file
-  res.send('hello, world!');
+  res.sendFile(join(__dirname, 'tmp.html'));
 });
 
 app.post('/input', (req, res) => {
