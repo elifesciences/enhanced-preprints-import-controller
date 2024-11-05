a9046866d56d84e70d3e27a368a24abfe10c7396
A. K <74007089+soggy-mushroom@users.noreply.github.com>
2024-11-05 12:18:50 +0000

    Install temporal and make a worflow request

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import express, { Express } from 'express';
 import { join } from 'path';
 import BodyParser from 'body-parser';
+import { Client } from '@temporalio/client';
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(BodyParser.urlencoded());
@@ -16,12 +17,20 @@ app.get('/input', (_, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
-app.post('/input', (req, res) => {
+app.post('/input', async (req, res) => {
   const form = req.body;
   console.log(`received form response: ${JSON.stringify(form)}`);
   // validate
   // send to temporal
-  res.send('import successful');
+  const client = new Client();
+  const workflow = await client.workflow.start('importManuscriptData', {
+    taskQueue: 'epp',
+    workflowId: 'your-workflow-id',
+    args: [
+      {}
+    ]
+  });
+  res.send(`import started ${workflow.workflowId} ${workflow.firstExecutionRunId}`);
 });
 
 app.get('/previous-imports', (_, res) => {
